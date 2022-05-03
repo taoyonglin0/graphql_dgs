ac5ce967c2a0cac120b625e32eccd81bf53a3766
taoyonglin <18500651411@163.com>
2022-05-03 16:46:57 +0800

    [init]

---
+++ src/main/java/com/example/graphql_dgs/GraphqlDgsApplication.java
@@ -0,0 +1,13 @@
+package com.example.graphql_dgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlDgsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlDgsApplication.class, args);
+    }
+
+}
